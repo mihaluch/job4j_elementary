6425e7d2d36119b0575648f85b088c662a6ef972
mihaluch <lm333@mail.ru>
2020-12-22 15:00:45 +0300

    4.1. Аргументы [#435589]

---
+++ src/ArgMethod.java
@@ -0,0 +1,14 @@
+public class ArgMethod {
+
+    public static void hello(String name) {
+        System.out.println("Hello, " + name);
+    }
+
+    public static void main(String[] args) {
+        String name = "Mikhail Lozovikov";
+
+        ArgMethod.hello(name);
+
+
+    }
+}
