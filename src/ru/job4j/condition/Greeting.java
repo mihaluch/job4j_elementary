020e55c8aba1056c24cdfa0f8b1120fe8ce7f381
mihaluch <lm333@mail.ru>
2020-12-23 22:58:41 +0300

    9.2. Checkstyle [#435594]

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -4,8 +4,6 @@ public class Greeting {
     public static void main(String[] args) {
         String idea = "I like Java!";
 
-
-
         System.out.println(idea + " But I am a newbie.");
 
         int year = 2020;
