7ead5ec67c4a6ecceb0ed9cd780afe6b1b24cd81
mihaluch <lm333@mail.ru>
2021-01-04 22:38:33 +0300

    4.6.2. Switch - matcher [#435535]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -2,12 +2,19 @@ package ru.job4j.condition;
 
 public class SqArea {
     public static double square(int p, int k) {
-        double rsl = (p / (2 * (k + 1))) * k;
-        return rsl;
+       double h = p / (2 * (k + 1));
+
+       double  L = h * k;
+
+       double s = L * h;
+
+
+        //double rsl = (p / (2 * (k + 1))) * k;
+        return s;
     }
 
     public static void main(String[] args) {
-        double result1 = SqArea.square(12, 3);
+        double result1 = SqArea.square(6, 2);
         System.out.println(" p = 12, k = 3, s = 2, real = " + result1);
     }
 
