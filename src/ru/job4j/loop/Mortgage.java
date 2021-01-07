03c22e50b5d6440265d2effaed8aaa40821d9727
mihaluch <lm333@mail.ru>
2021-01-07 12:50:14 +0300

    5.7. Ипотека [#435637]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -3,14 +3,11 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(int amount, int salary, double percent) {
         int year = 0;
-        double debt = 1;
+        double debt = amount;
         //System.out.println(debt);
         while (debt > 0) {
-            if (year == 0) {
-                debt = amount + amount * percent / 100;
-            }
-            debt = (debt - salary);
             debt = debt + debt * percent / 100;
+            debt = (debt - salary);
             year += 1;
         }
         return year;
